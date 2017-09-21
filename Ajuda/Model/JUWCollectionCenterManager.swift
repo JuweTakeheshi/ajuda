94eb47bdccb3c3e78981ed69b65fe1fea5b936cf
Juwe Takeheshi <juwe81@gmail.com>
2017-09-21 20:02:35 -0300

    Realm framework added

---
+++ Ajuda/Model/JUWCollectionCenterManager.swift
@@ -0,0 +1,13 @@
+//
+//  JUWCollectionCenterManager.swift
+//  Ajuda
+//
+//  Created by Juwe Takeheshi on 9/21/17.
+//  Copyright © 2017 Juwe Takeheshi. All rights reserved.
+//
+
+import UIKit
+
+class JUWCollectionCenterManager: NSObject {
+    
+}
