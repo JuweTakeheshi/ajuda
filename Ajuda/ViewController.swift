9047f6d8cf5ecea988411eb0f97e01cab4f63f8c
Juwe Takeheshi <juwe81@gmail.com>
2017-09-21 13:53:40 -0300

    Initial Commit

---
+++ Ajuda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ajuda
+//
+//  Created by Juwe Takeheshi on 9/21/17.
+//  Copyright © 2017 Juwe Takeheshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
