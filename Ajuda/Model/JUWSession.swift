b27dcbea6ba3d8048fe74ebea568c8745b4be80d
Juwe Takeheshi <juwe81@gmail.com>
2017-09-21 15:04:27 -0300

    UI first screen

---
+++ Ajuda/Model/JUWSession.swift
@@ -0,0 +1,17 @@
+//
+//  JUWSession.swift
+//  Ajuda
+//
+//  Created by Juwe Takeheshi on 9/21/17.
+//  Copyright © 2017 Juwe Takeheshi. All rights reserved.
+//
+
+import UIKit
+
+class JUWSession: NSObject {
+    static let sharedInstance = JUWSession()
+
+    func isValid() -> Bool {
+        return true
+    }
+}
---
+++ Ajuda/Model/JUWUser.swift
@@ -0,0 +1,13 @@
+//
+//  JUWUser.swift
+//  Ajuda
+//
+//  Created by Juwe Takeheshi on 9/21/17.
+//  Copyright © 2017 Juwe Takeheshi. All rights reserved.
+//
+
+import UIKit
+
+class JUWUser: NSObject {
+    var userName: String?
+}
