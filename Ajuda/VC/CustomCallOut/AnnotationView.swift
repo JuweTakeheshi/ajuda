b22a5fde4bc5ced00feae1289edc56c525b73959
Ricardo Hernandez <ricardohernandez@2806-106e-000d-3069-1c53-b595-9af3-8e80.ipv6.infinitum.net.mx>
2017-09-23 00:57:16 -0500

    -Custom CallOut is ready for every iOS OS with Xib

---
+++ Ajuda/VC/CustomCallOut/AnnotationView.swift
@@ -0,0 +1,37 @@
+//
+//  AnnotationView.swift
+//  Ajuda
+//
+//  Created by Ricardo Hernandez on 22/09/17.
+//  Copyright © 2017 Juwe Takeheshi. All rights reserved.
+//
+
+import MapKit
+
+class AnnotationView: MKAnnotationView{
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        if (hitView != nil)
+        {
+            self.superview?.bringSubview(toFront: self)
+        }
+        return hitView
+    }
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let rect = self.bounds;
+        var isInside: Bool = rect.contains(point);
+        if(!isInside)
+        {
+            for view in self.subviews
+            {
+                isInside = view.frame.contains(point);
+                if isInside
+                {
+                    break;
+                }
+            }
+        }
+        return isInside;
+    }
+}
