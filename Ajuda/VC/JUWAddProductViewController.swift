e0aece6a5145ac0c2daef0b7ea87f60f9874d720
Josué Hernández Ramírez <ingscjoshua@gmail.com>
2017-09-25 14:35:28 -0500

    Integration changes: fix add products, refactoring

--- Ajuda/VC/JUWAddProductViewController.swift
+++ Ajuda/VC/JUWAddProductViewController.swift
@@ -18,6 +18,10 @@ class JUWAddProductViewController: UIViewController {
         super.viewDidLoad()
         addProductTextField.delegate = self
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.displayOKAlert(title: "Aviso", message: "Estás a punto de agregar un producto que este centro de acopio necesita, por favor introduce solamente información verídica y confirmada")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -34,30 +38,18 @@ class JUWAddProductViewController: UIViewController {
     
     func showAddProductAlert() {
         // Mostrar alerta al usuario pregntando si está seguro que esto que se va a agregar está confirmado como requerido en el centro de acopio...
-        let message = UIAlertController(title: "Confirmacion", message: "¿Realmente desea agregar este producto a la lista de necesidades para este centro de acopio?", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Aceptar", style: .default){ action in
+        self.displayConfirmHandlAlert(title: "Confirmacion", message: "¿Realmente desea agregar este producto a la lista de necesidades para este centro de acopio?"){ action in
             self.addProduct(product:self.addProductTextField.text!)
         }
-        let cancelAction = UIAlertAction(title: "Cancelar", style: .destructive, handler:nil)
-        message.addAction(cancelAction)
-        message.addAction(okAction)
-        self.present(message, animated: true, completion: nil)
+        
     }
     
     func addProduct(product:String){
-        let networkManager  = JUWCollectionCenterManager()
-        networkManager.addProductToCollectionCenter(collectionCenter: currentCenter, product: product, completion:{
-            let message = UIAlertController(title: "Aviso", message: "Producto agregado correctamente", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Aceptar", style: .default){ action in
-                self.parent?.dismiss(animated: true, completion: nil)
-            }
-            message.addAction(okAction)
-            self.present(message, animated: true, completion: nil)
+        let collectionCenterManager  = JUWCollectionCenterManager()
+        collectionCenterManager.addProductToCollectionCenter(collectionCenter: currentCenter, product: product, completion:{
+            self.displayOKAlert(title: "Aviso", message: "Producto agregado correctamente")
         }, failure:{ error in
-            let message = UIAlertController(title: "Aviso", message: "Tuvimos un problema al tratar de añadir este producto intenta mas tarde", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Aceptar", style: .default,handler:nil)
-            message.addAction(okAction)
-            self.present(message, animated: true, completion: nil)
+             self.displayOKAlert(title: "Aviso", message: "Tuvimos un problema al tratar de añadir este producto intenta mas tarde")
         })
     }
 }
