abdf60803436bbdcc1b50141a53186c83dc684bf
Juwe Takeheshi <juwe81@gmail.com>
2017-09-25 16:38:26 -0300

    delete in keychain method

--- Ajuda/Model/keychain/JUWKeychainService.swift
+++ Ajuda/Model/keychain/JUWKeychainService.swift
@@ -36,6 +36,7 @@ public class JUWKeychainService: NSObject {
      * Exposed methods to perform save and load queries.
      */
 
+    //MARK: - Token
     public class func saveToken(token: NSString) {
         self.save(service: tokenKey as NSString, data: token)
     }
@@ -44,23 +45,41 @@ public class JUWKeychainService: NSObject {
         return self.load(service: tokenKey as NSString)
     }
 
+    public class func deleteToken() {
+        self.delete(service: tokenKey as NSString)
+    }
+
+    //MARK: - User Type
     public class func saveUserType(type: NSString) {
         self.save(service: userTypeKey as NSString, data: type)
     }
-
     public class func loadUserType() -> NSString? {
         return self.load(service: userTypeKey as NSString)
     }
 
-    /**
-     * Internal methods for querying the keychain.
-     */
-    
+    //MARK: - Internal methods
+
+    private class func delete(service: NSString) {
+        
+//        let dataFromString: NSData = data.data(using: String.Encoding.utf8.rawValue, allowLossyConversion: false)! as NSData
+
+        /* return @{
+         (__bridge id)kSecClass : (__bridge id)kSecClassGenericPassword,
+         (__bridge id)kSecAttrService : kSkyBellKeychainService,
+         (__bridge id)kSecAttrAccount : identifier,
+         (__bridge id)kSecAttrAccessible : (__bridge id)kSecAttrAccessibleWhenUnlocked
+         };*/
+        // Instantiate a new default keychain query
+//        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecValueDataValue, service, userAccount, dataFromString], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecValueDataValue])
+//
+//        SecItemDelete(keychainQuery as CFDictionary)
+    }
+
     private class func save(service: NSString, data: NSString) {
         let dataFromString: NSData = data.data(using: String.Encoding.utf8.rawValue, allowLossyConversion: false)! as NSData
         
         // Instantiate a new default keychain query
-        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, service, userAccount, dataFromString], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecValueDataValue])
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecValueDataValue, service, userAccount, dataFromString], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecValueDataValue])
         
         // Delete any existing items
         SecItemDelete(keychainQuery as CFDictionary)
@@ -73,7 +92,7 @@ public class JUWKeychainService: NSObject {
         // Instantiate a new default keychain query
         // Tell the query to return a result
         // Limit our results to one item
-        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, service, userAccount, kCFBooleanTrue, kSecMatchLimitOneValue], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecReturnDataValue, kSecMatchLimitValue])
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecValueDataValue, service, userAccount, kCFBooleanTrue, kSecMatchLimitOneValue], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecReturnDataValue, kSecMatchLimitValue])
         
         var dataTypeRef :AnyObject?
         
