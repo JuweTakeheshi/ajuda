493c305867fd52cf93f82b1eadacda8a8d19f46d
Josué Hernández Ramírez <ingscjoshua@gmail.com>
2017-09-26 14:53:48 -0500

    integration changes: fix acces_token on request

--- Ajuda/Model/JUWConstants.swift
+++ Ajuda/Model/JUWConstants.swift
@@ -10,10 +10,10 @@ import Foundation
 
 let kUserAuthenticationUrl = "http://hapi.balterbyte.com:8080/api/voluntarios/login"
 let kUserCreationUrl = "http://hapi.balterbyte.com:8080/api/voluntarios"
-let kCollectionCentersUrl = "http://hapi.balterbyte.com:8080/api/acopios"
-let kCollectionCenterNeedsUrl = "http://hapi.balterbyte.com:8080/api/acopios/%@/productos"
-let kCollectionCenterContactInfoUrl = "http://hapi.balterbyte.com:8080/api/acopios/%@/contactos"
-let kCollectionCenterSearchProductUrl = "http://hapi.balterbyte.com:8080/api/productos?filter={\"where\":{\"nombre\":{\"like\":\"%@\"}}}"
+let kCollectionCentersUrl = "http://hapi.balterbyte.com:8080/api/acopio?access_token=%@s"
+let kCollectionCenterNeedsUrl = "http://hapi.balterbyte.com:8080/api/acopios/%@/productos?access_token=%@"
+let kCollectionCenterContactInfoUrl = "http://hapi.balterbyte.com:8080/api/acopios/%@/contactos?access_token=%@"
+let kCollectionCenterSearchProductUrl = "http://hapi.balterbyte.com:8080/api/productos?filter={\"where\":{\"nombre\":{\"like\":\"%@\"}}}?access_token=%@"
 let kCollectionCenterAddProduct = "http://hapi.balterbyte.com:8080/api/acopios/%@/aceptan?access_token=%@"
 
 let kTokenKey = "KeyForToken"
